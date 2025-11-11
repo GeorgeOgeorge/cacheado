2014fae62cdcf05702f17c3d02c14687ebf90929
George Morais <george.morais@zenvia.com>
2025-11-11 01:29:29 -0300

    feat: bumps code version

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from .cache_types import _CacheKey, _CacheScope, _CacheValue
 from .eviction_policies.lre_eviction import LRUEvictionPolicy
 from .storages.in_memory import InMemory
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 __all__ = [
     "Cache",
